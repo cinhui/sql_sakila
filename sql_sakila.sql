7c24eb67b2e53b1438bceafebe13195620762547
cinhui <45111514+cinhui@users.noreply.github.com>
2019-04-16 11:25:14 -0500

    Cleaned up sql file

--- sql_sakila.sql
+++ sql_sakila.sql
@@ -1,21 +1,34 @@
 USE sakila;
   
 -- 1a. Display the first and last names of all actors from the table actor.
-SELECT first_name, last_name FROM actor;
+SELECT first_name, last_name 
+FROM actor;
 -- 1b. Display the first and last name of each actor in a single column in upper case letters. 
 -- Name the column Actor Name.
-SELECT CONCAT(first_name,' ', last_name) AS 'Actor Name' FROM actor;
+SELECT CONCAT(first_name,' ', last_name) AS 'Actor Name' 
+FROM actor;
+
 -- 2a. You need to find the ID number, first name, and last name of an actor, 
 -- of whom you know only the first name, "Joe." What is one query would you use to obtain this information?
-SELECT actor_id, first_name, last_name FROM actor WHERE first_name = 'Joe';
+SELECT actor_id, first_name, last_name 
+FROM actor 
+WHERE first_name = 'Joe';
 -- 2b. Find all actors whose last name contain the letters GEN:
-SELECT * FROM actor WHERE last_name LIKE '%GEN%';
+SELECT * 
+FROM actor 
+WHERE last_name LIKE '%GEN%';
 -- 2c. Find all actors whose last names contain the letters LI. 
 -- This time, order the rows by last name and first name, in that order:
-SELECT * FROM actor WHERE last_name LIKE '%LI%' ORDER BY last_name, first_name;
+SELECT * 
+FROM actor 
+WHERE last_name LIKE '%LI%' 
+ORDER BY last_name, first_name;
 -- 2d. Using IN, display the country_id and country columns of the following countries: 
 -- Afghanistan, Bangladesh, and China:
-SELECT country_id, country FROM country WHERE country IN ('Afghanistan','Bangladesh','China');
+SELECT country_id, country 
+FROM country 
+WHERE country IN ('Afghanistan','Bangladesh','China');
+
 -- 3a. You want to keep a description of each actor. You don't think you will be performing queries 
 -- on a description, so create a column in the table actor named description and use the data type BLOB 
 -- (Make sure to research the type BLOB, as the difference between it and VARCHAR are significant).
@@ -23,44 +36,62 @@ ALTER TABLE actor ADD description BLOB;
 -- 3b. Very quickly you realize that entering descriptions for each actor is too much effort. 
 -- Delete the description column.
 ALTER TABLE actor DROP COLUMN description;
+
 -- 4a. List the last names of actors, as well as how many actors have that last name.
-SELECT last_name, count(*) FROM actor GROUP BY last_name;
+SELECT last_name, count(*) 
+FROM actor 
+GROUP BY last_name;
 -- 4b. List last names of actors and the number of actors who have that last name, 
 -- but only for names that are shared by at least two actors
-SELECT last_name, count(*) FROM actor GROUP BY last_name HAVING count(*) > 1;
+SELECT last_name, count(*) 
+FROM actor 
+GROUP BY last_name HAVING count(*) > 1;
 -- 4c. The actor HARPO WILLIAMS was accidentally entered in the actor table as GROUCHO WILLIAMS. 
 -- Write a query to fix the record.
-UPDATE actor SET first_name = 'HARPO' WHERE first_name = 'GROUCHO' AND last_name = 'Williams';
+UPDATE actor SET first_name = 'HARPO' 
+WHERE first_name = 'GROUCHO' AND last_name = 'Williams';
 -- 4d. Perhaps we were too hasty in changing GROUCHO to HARPO. 
 -- It turns out that GROUCHO was the correct name after all! 
 -- In a single query, if the first name of the actor is currently HARPO, change it to GROUCHO.
-UPDATE actor SET first_name = 'GROUCHO' WHERE first_name = 'HARPO' AND last_name = 'Williams';
-UPDATE actor SET first_name = 'GROUCHO' WHERE actor_id = 172;
+UPDATE actor SET first_name = 'GROUCHO' 
+WHERE first_name = 'HARPO' AND last_name = 'Williams';
+UPDATE actor SET first_name = 'GROUCHO' 
+WHERE actor_id = 172;
+
 -- 5a. You cannot locate the schema of the address table. Which query would you use to re-create it?
 -- Hint: https://dev.mysql.com/doc/refman/5.7/en/show-create-table.html
 SHOW CREATE TABLE address;
+
 -- 6a. Use JOIN to display the first and last names, as well as the address, of each staff member. 
 -- Use the tables staff and address:
-SELECT first_name, last_name, address FROM staff INNER JOIN address
+SELECT first_name, last_name, address 
+FROM staff INNER JOIN address
 ON staff.address_id = address.address_id;
 -- 6b. Use JOIN to display the total amount rung up by each staff member in August of 2005. 
 -- Use tables staff and payment.
-SELECT staff.staff_id, sum(amount) FROM payment INNER JOIN staff
+SELECT staff.staff_id, sum(amount) 
+FROM payment INNER JOIN staff
 ON payment.staff_id = staff.staff_id 
-WHERE payment_date like '2005-08-%' GROUP BY payment.staff_id;
+WHERE payment_date like '2005-08-%' 
+GROUP BY payment.staff_id;
 -- 6c. List each film and the number of actors who are listed for that film. 
 -- Use tables film_actor and film. Use inner join.
-SELECT title, count(*) FROM film INNER JOIN film_actor
+SELECT title, count(*) 
+FROM film INNER JOIN film_actor
 ON film.film_id = film_actor.film_id 
 GROUP BY film.film_id;
 -- 6d. How many copies of the film Hunchback Impossible exist in the inventory system?
-SELECT title, count(*) FROM inventory INNER JOIN film
+SELECT title, count(*) 
+FROM inventory INNER JOIN film
 ON film.film_id = inventory.film_id
 WHERE film.title = 'Hunchback Impossible';
 -- 6e. Using the tables payment and customer and the JOIN command, list the total paid by each customer. 
 -- List the customers alphabetically by last name:
-SELECT customer.first_name, customer.last_name, sum(amount) AS 'Total Amount Paid' FROM payment INNER JOIN customer
-ON customer.customer_id = payment.customer_id GROUP BY customer.customer_id ORDER BY customer.last_name;
+SELECT customer.first_name, customer.last_name, sum(amount) AS 'Total Amount Paid' 
+FROM payment INNER JOIN customer
+ON customer.customer_id = payment.customer_id 
+GROUP BY customer.customer_id ORDER BY customer.last_name;
+
 -- 7a. The music of Queen and Kris Kristofferson have seen an unlikely resurgence. 
 -- As an unintended consequence, films starting with the letters K and Q have also soared in popularity. 
 -- Use subqueries to display the titles of movies starting with the letters K and Q whose language is English.
